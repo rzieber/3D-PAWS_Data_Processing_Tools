69f6e5d66e74db9cb6e0720d072d294b4a7b537d
rebecca-zieber <rzieber@ucar.edu>
2024-12-17 13:34:52 -0700

    Data gap infill upgrades

--- data_gap_filler.py
+++ data_gap_filler.py
@@ -2,34 +2,38 @@ from pandas import Index, Timestamp
 import pandas as pd
 import numpy as np
 from datetime import timedelta
-import sys
 
 """
-Helper function to fill_empty_rows() 
-Creates an array representing the row to be added for infilling a data gap. The array contains the missing timestamp, 
-and np.nan's for each of the dataframe's columns.
+Helper function to fill_empty_rows()
+Builds a row to be merged with the dataframe for a missing timestamp.
+Uses the dataframe's column order.
 
 columns -- the df's column attribute
 missing_timestamp -- the timestamp for the missing row
+fill_empty -- specify what value should populate cells, np.nan by default [OPTIONAL] 
 """
-def _build_empty_row(columns:Index, missing_timestamp:Timestamp) -> list:
+def _build_empty_row(columns:Index, missing_timestamp:Timestamp, fill_empty=np.nan) -> list:
     if not isinstance(columns, Index):
         raise TypeError(f"The build_empty_row() function expects 'columns' parameter to be of type <Index>, passed: {type(columns)}")
     if not isinstance(missing_timestamp, Timestamp):
         raise TypeError(f"The build_empty_row() function expects 'missing_timestamp' parameter to be of type <Timestamp>, passed: {type(missing_timestamp)}")
 
-    next_row = [missing_timestamp]
+    next_row = []
+
     for col in columns:
-        if col == 'date':
+        if col == 'date': 
+            next_row.append(missing_timestamp)
             continue
 
-        next_row.append(np.nan)
+        next_row.append(fill_empty)
 
     return next_row
 
+
 """
-Creates a list to merge with the dataframe that fills in data gaps with missing timestamps. 
-Merges that list with the dataframe to be reformatted and returns it. 
+Create a list to merge with the reformatted dataframe that fills in data gaps with missing timestamps. 
+Merges that list with the reformatted dataframe, sorts by time, and returns it. 
+REQUIREMENT: the timestamp column must be named 'date' ---------------------------------------------------
 
 reformatted_df -- the df to reformat with data gaps filled
 time_delta -- the dataset's resolution (1-minute reporting period)
@@ -42,6 +46,9 @@ def fill_empty_rows(reformatted_df:pd.DataFrame, time_delta:timedelta, set_index
         raise ValueError(f"The fill_empty_row() function expects the 'time_delta' parameter to be of type <timedelta>, received: {type(time_delta)}")
     if not isinstance(set_index, bool):
         raise ValueError(f"The fill_empty_row() function expects the 'set_index' parameter to be of type <bool>, received: {type(set_index)}")
+
+    nan_columns = reformatted_df.columns[reformatted_df.isna().all()]
+    if not nan_columns.empty: print("fill_empty_rows() -- Columns with all NaN values:", nan_columns.tolist())
 
     if 'date' not in reformatted_df.columns: reformatted_df.reset_index(inplace=True)
 
@@ -56,14 +63,18 @@ def fill_empty_rows(reformatted_df:pd.DataFrame, time_delta:timedelta, set_index
             new_row = _build_empty_row(reformatted_df.columns, current_timestamp)
             if new_row is not None:
                 blank_rows.append(new_row)
-            
-    if blank_rows:
+
+    if not blank_rows: return reformatted_df      
+    else:
         blank_rows_df = pd.DataFrame(blank_rows, columns=reformatted_df.columns)
         reformatted_df = pd.concat([reformatted_df, blank_rows_df]).sort_values('date').reset_index(drop=True)
 
-    if set_index: reformatted_df.set_index('date', inplace=True)
+        reformatted_df.sort_values(by='date', inplace=True)
 
-    return reformatted_df
+        if set_index: reformatted_df.set_index('date', inplace=True)
+
+        return reformatted_df
+
 
 
 """
